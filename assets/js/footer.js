97723bea41a7612e2d80515cfcf7307d1dc39de3
Alfan Rozak <93852663+alfanzaky@users.noreply.github.com>
2025-07-02 20:38:49 +0700

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const footerContainer = document.getElementById("footerContainer");
 
-  if (footerContainer) {
-    try {
-      const response = await fetch("/jurnal-trading/components/footer.html");
-      const html = await response.text();
-      footerContainer.innerHTML = html;
-    } catch (err) {
-      console.error("Gagal load footer:", err);
-    }
+  if (!footerContainer) return;
+
+  try {
+    const response = await fetch("/jurnal-trading/components/footer.html");
+    if (!response.ok) throw new Error("Gagal memuat footer");
+    const html = await response.text();
+    footerContainer.innerHTML = html;
+  } catch (err) {
+    console.error("Gagal load footer:", err);
   }
 });
